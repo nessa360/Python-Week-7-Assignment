92b333a102e62ab9ac53690b6f725583fcf0f21f
nessa360 <baahwilliamsvanessa@gmail.com>
2025-05-13 07:46:10 +0000

    Initial commit

---
+++ .ipython/profile_default/startup/00-env.py
@@ -0,0 +1,17 @@
+# *WARNING* Do not change this file as it may break your notebooks
+
+import os
+os.environ['HOME'] = '/home/f0d09a11-e1b2-488f-879b-9635c367975f'
+os.environ['LC_CTYPE'] = 'en_US.utf-8'
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java'
+os.environ['TERM'] = 'xterm-256color'
+os.environ['PYTHONSTARTUP'] = '/home/f0d09a11-e1b2-488f-879b-9635c367975f/.pythonstartup.py'
+os.environ['PYTHONANYWHERE_DOMAIN'] = 'anacondaapps.cloud'
+os.environ['PYTHONANYWHERE_SITE'] = 'nb.anaconda.cloud'
+os.environ['ANACONDA_ASSISTANT_API_URL'] = 'https://assistant.anaconda.cloud'
+os.environ['ANACONDA_ASSISTANT_ENVIRONMENT_TYPE'] = 'cloud-notebooks-prod'
+os.environ['ANACONDA_PROJECTS_DIR'] = 'anaconda_projects'
+os.environ['http_proxy'] = 'http://proxy.server:3128'
+os.environ['https_proxy'] = 'http://proxy.server:3128'
+os.environ['no_proxy'] = 'localhost,127.0.0.1,localaddress,.localdomain.com,/var/run/docker.sock'
+os.environ['API_TOKEN'] = '11f873e7e39df72b33cf9f2690e1f7a8449336ca'
